9d035b4f9604f45f673c874f1e577380a5ac6aaf
AlexanderImhof <alexander.m.imhof@googlemail.com>
2023-06-04 23:09:03 +0200

    Commit GUI

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import flet as ft
 
-def main(page: ft.page):
-    t = ft.Text(value="Hallo Benutzer", color="#3776A1")
-    page.add(t)
 
-ft.app(target=main)
+def main(page: ft.page):
+    page.title = "Yoginator"
+    page.window_center()
+    page.bgcolor = ft.colors.TEAL_100
+    page.update()
+
+    username = ft.TextField(label="username", autofocus=True, )
+    password = ft.TextField(label="password")
+    signin = ft.ElevatedButton(text="sign in", icon=f"IconLogIn.png", icon_color="#3776A1")
+    divider = ft.Divider(height=1, thickness=1, color=ft.colors.TEAL_700)
+    signup = ft.ElevatedButton(text="sign up", icon="IconLogIn", icon_color="#3776A1")
+
+    page.add(ft.Column(controls=[username, password, signin, divider, signup], horizontal_alignment=ft.CrossAxisAlignment.CENTER, alignment=ft.MainAxisAlignment.CENTER,width=500, height=20))
+
+if __name__ == "__main__":
+    ft.app(target=main)
