e6808fb4d8509c8c5c1d674d1a8ecd73b66c4e1f
AlexanderImhof <alexander.m.imhof@googlemail.com>
2023-06-05 11:03:41 +0200

    Commit GUI

--- main.py
+++ main.py
@@ -20,26 +20,21 @@ def main(page: ft.page):
                             ]
                             )
 
-    divider = ft.Divider(height=1, thickness=1, color=ft.colors.TEAL_700)
-    signup = ft.ElevatedButton(text="sign up", icon="IconLogIn", icon_color="#3776A1")
-
     page.add(
         ft.Column(
             controls=[
                 ft.TextField(label="username", border_radius=10, content_padding=20 , autofocus=True, bgcolor=ft.colors.TEAL_100, focused_bgcolor=ft.colors.TEAL_ACCENT_400),
                 ft.TextField(label="password", border_radius=10, content_padding=20 , bgcolor=ft.colors.TEAL_100, focused_bgcolor=ft.colors.TEAL_ACCENT_400),
-                ft.IconButton(ft.icons.LOGIN, icon_color=ft.colors.TEAL_700, bgcolor=ft.colors.TEAL_ACCENT_400)
+                ft.IconButton(ft.icons.LOGIN, icon_color=ft.colors.TEAL_700, bgcolor=ft.colors.TEAL_ACCENT_400),
+                ft.Divider(height=300, thickness=0, color=ft.colors.TEAL_700),
+                ft.Row(controls=[ft.Text(value="Don't have an account?"), ft.TextButton(text="sign up")], alignment=ft.MainAxisAlignment.CENTER, vertical_alignment=ft.CrossAxisAlignment.CENTER )
             ],
-            alignment=ft.MainAxisAlignment.START, horizontal_alignment=ft.CrossAxisAlignment.CENTER
-        ),
-        ft.Row(
-            controls=[
-                ft.Text(value="Don't have an account?", width=400, height=50),
-                ft.TextButton(text="sign up", width=400, height=50)
-            ],
-            alignment=ft.MainAxisAlignment.CENTER, vertical_alignment=ft.CrossAxisAlignment.CENTER
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER
         )
     )
+
+
 
 
 if __name__ == "__main__":
